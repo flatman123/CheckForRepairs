0e4736dab26800844a5eb71f903af2fc810dd901
jeffrey mcintyre <flatlandpyhon67@hotmail.com>
2019-08-16 14:57:13 -0700

    added generateREport promise

--- customerReport.js
+++ customerReport.js
@@ -24,20 +24,52 @@ const prntCustName = () => {
 	})
 };
 
-const checkNextRepairDate = () => {
+const checkForlastRepair = () => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 
 			let oldDate = new Date(customers.get('cust1')[1]['repairDate']);
 			let upToDate = new Date('5/1/2019');
-			oldDate < upToDate ? resolve('CONTACT CUSTOMER FOR REPAIRS') : resolve('NO REPAIRS NEEDED FOR THIS CUSTOMER.');
+			oldDate < upToDate ? resolve(true) : resolve(false);
+			
 		}, 2000)
+
 	}
 )};
+
+const generateReport = nr => {
+	return new Promise((resolve, reject) => {
+		setTimeout(repair => {
+			let cName, bike, repairDate, phone;
+
+			cName = customers.get('cust1')[3]['name'];
+			bike = customers.get('cust1')[2]['bicycleType'];
+			repairDate = customers.get('cust1')[1]['repairDate'];
+			phone = customers.get('cust1')[0]['phone'];
+			
+			if (repair) {
+				resolve(`Customer ${cName} is in need of some repairs. Please contact them at: ${phone}.The last time they had a repair was ${repairDate} for their ${bike} bike.`);
+			} else {
+				resolve(`Customer ${cName} is ok. No repairs needed.`);
+			}
+		}, 1500,nr)
+	})
+};
+
 
 const names = prntCustName();
 
 names
-.then(d => {console.log(d)return checkNextRepairDate();})
-.then(repairs => {console.log(repairs);})
+.then(d => {
+	console.log(d);
+	return checkForlastRepair();})
+
+.then(n => {
+	return generateReport(n);
+})
+
+.then(report => {
+	console.log(report);
+})
+
 .catch(() => {console.log('ERROS')});
